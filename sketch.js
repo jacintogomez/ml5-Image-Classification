8f05d06be0dc56da19ae430cd4ce412198c66419
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-03-13 22:29:48 -0400

    continuous calling of prediction

--- sketch.js
+++ sketch.js
@@ -1,33 +1,38 @@
 let mobilenet;
-let img;
+let video;
+let label='',prob='';
+video.crossOrigin='Anonymous';
 
 function modelready(){
     console.log('Model is ready!');
-    mobilenet.predict(img,gotresults)
+    mobilenet.predict(gotresults);
 }
 
 function gotresults(error,results){
     if(error){console.error(error);}
     else{
         console.log(results);
-        let label=results[0].label;
-        let prob=results[0].confidence.toFixed(3);
-        fill(0);
-        textSize(64);
-        text(`This is a ${label}`,10,50);
-        text(`With ${prob*100}% confidence`,10,height-30);
-        createP(`Label: ${label}`);
-        createP(`Probability: ${prob}`);
+        label=results[0].label;
+        prob=results[0].confidence.toFixed(3);
         console.log('Done');
+        setTimeout(mobilenet.predict(gotresults),1000);
     }
 }
 
-function imageready(){image(img,0,0,width,height);}
+function draw(){
+    image(video,0,0);
+    fill(0);
+    textSize(64);
+    text(`This is a ${label}`,10,50);
+    text(`With ${prob*100}% confidence`,10,height-30);
+}
+
+//function imageready(){image(img,0,0,width,height);}
 function setup(){
     createCanvas(640,480);
     let imgname='img/sloth.jpg'
-    img=createImg(imgname,imageready);
-    img.hide();
+    video=createCapture(VIDEO);
+    video.hide();
     background(0);
-    mobilenet=ml5.imageClassifier('MobileNet',modelready);
+    mobilenet=ml5.imageClassifier('MobileNet',video,modelready);
 }
