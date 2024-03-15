1c5094debc97307222f70c97a33b030902576e90
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-03-15 01:19:43 -0400

    object detection with label boxes, scaled to photo size

--- detection.js
+++ detection.js
@@ -3,7 +3,7 @@ let detector;
 //img.crossOrigin='Anonymous';
 
 function preload(){
-    img=loadImage('img/dogsnake.jpg');
+    img=loadImage('img/jack.jpeg');
     detector=ml5.objectDetector('cocossd');
 }
 
@@ -12,19 +12,23 @@ function gotdetections(error,results){
     console.log(results);
     for(let i=0;i<results.length;i++){
         let ob=results[i];
+        let xnorm=ob.normalized.x*width;
+        let ynorm=ob.normalized.y*height;
+        let wnorm=ob.normalized.width*width;
+        let hnorm=ob.normalized.height*height;
         stroke(0,255,0);
         strokeWeight(4);
         noFill();
-        rect(ob.x,ob.y,ob.width,ob.height);
+        rect(xnorm,ynorm,wnorm,hnorm);
         noStroke();
         fill(255);
         textSize(24);
-        text(ob.label,ob.x+10,ob.y+24);
+        text(ob.label+', '+(ob.confidence*100).toFixed(2)+'% confidence',xnorm+10,ynorm+24);
     }
 }
 
 function setup(){
     createCanvas(640,480);
-    image(img,0,0);
+    image(img,0,0,width,height);
     detector.detect(img,gotdetections);
 }
